861f6d7da0fd7c2f2c3b92d55d6a2640ff38ce7e
Anton Lysenko <achesco@gmail.com>
2013-10-30 16:35:07 +0200

    Comments putted in order.

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var express = require('express')
 
 var app = express();
 
-// Emulating sourceMapPrefix option, for css source maps.
+// Emulating sourceMapPrefix option. The number of directories to drop
+// from the path prefix when declaring files in the source map.
 var cssSourceMapPrefix = 2;
 app.use(function (req, res, next) {
 	var uris = req.url.split('/');
+	// Customize conditions to meet your requirements
 	if (uris.length > cssSourceMapPrefix && uris[cssSourceMapPrefix+1] == 'css') {
 		var url = '/' + uris.splice(cssSourceMapPrefix+1).join('/');
 		console.log('CSS source map prefix rewrite from ' + req.url + ' to ' + url);
@@ -26,14 +28,3 @@ http.createServer(app).listen('8080', function () {
 	console.log("Express server listening on port 8080");
 });
 
-
-//
-//app.use(function(req, res, next){
-//	check if req.url is of '/blog/*
-//	if(req.url.indexOf(/blog/) > -1){
-//		// then grab the last part and redirect it
-//		var postId = (req.url.split("/")[2] || "")
-//		req.url = '/posts/' + postId + '.html';
-//	}
-//	next()
-//})
